da71536771101238e057604f38b798a884d449c9
Joren Broekema <joren.broekema@gmail.com>
2018-11-08 11:53:58 +0000

    added interceptors demo

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+// Add a Request interceptor
+axios.interceptors.request.use((config) => {
+  // Do something before request is sent
+  config.timeout = 1500;
+  appendJSONToDOM(config, '.beforeRequest');
+  return config;
+}, (error) => {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a Response interceptor
+axios.interceptors.response.use((response) => {
+  // Do something with response data
+  response.statusText = "200OK hell yeah";
+  appendJSONToDOM(response, '.afterRequest');
+  return response;
+}, (error) => {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+// Removing an interceptor
+const myInterceptor = axios.interceptors.request.use(() => {});
+axios.interceptors.request.eject(myInterceptor);
+
+// Custom instance interceptor
+const instance = axios.create();
+instance.interceptors.request.use(() => {});
+
 axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://reqres.in/api/users/3`)
   .then((response) => {
-    document.querySelector('.container').innerHTML = `
-      <pre>${JSON.stringify(response.data, undefined, 2)}</pre>
-    `;
+    appendJSONToDOM(response, '.response');
   })
   .catch((error) => {
 
   });
+
+function appendJSONToDOM(data, container) {
+  const el = document.createElement('pre');
+  el.innerHTML = JSON.stringify(data, undefined, 2);
+  document.querySelector(container).appendChild(el);
+}
